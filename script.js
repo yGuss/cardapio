7b3f42398397013ab8226cea911e8c26df3d3c4f
yGuss <guuhhenrique159@gmail.com>
2024-07-17 15:20:05 -0300

    Fixed font bold

--- script.js
+++ script.js
@@ -307,7 +307,7 @@ function checkRestaurantOpen() {
 
     const hora = data.getHours();
 
-    return hora >= 10 && hora < 23 && ((dia === 5 || dia === 6 || dia === 3));
+    return hora >= 19 && hora < 23 && ((dia === 5 || dia === 6 || dia === 0));
 }
 
 const spanItem = document.getElementById('date-span')
